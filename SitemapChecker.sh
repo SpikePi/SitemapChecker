459e1a1375a2911fbc41b04d599165382cbc749b
rho <rho@localhost.localdomain>
2017-03-28 08:36:41 +0200

    New function to print out the meaning of status codes and some minor refacturing

--- SitemapChecker.sh
+++ SitemapChecker.sh
@@ -110,7 +110,7 @@ shuf sitemap_links_all.txt > sitemap_links_all.tmp
 clear
 
 # show and save timestamp to report
-date | tee SitemapCheck_Report_$label.txt
+date | tee Report.txt
 
 # check if there was a second argument for maxNumURLs
 if [ -z $3 ]; then
@@ -139,33 +139,133 @@ SECONDS=0
 linksCheck=$(wc -l < sitemap_links_part.txt)
 
 # create the header of the Report
-echo -e "\nTest\tResponse\tURL" | tee -a SitemapCheck_Report_$label.txt
+echo -e "\nTest\tResponse\tURL" | tee -a Report.txt
 
 # loop through all links in the file and test them
 for link in $(cat sitemap_links_part.txt); do
-    echo -en "$counter/$linksCheck\t" | tee -a SitemapCheck_Report_$label.txt
+    echo -en "$counter/$linksCheck\t" | tee -a Report.txt
     code=$(curl -L --max-time 10 --connect-timeout 5 --silent --head -o /dev/null -w "%{http_code}" $link)
-    echo -n $code | tee -a SitemapCheck_Report_$label.txt
+    echo -n $code | tee -a Report.txt
     if [ $code -gt 200 ]; then
         anomalies=$[$anomalies +1]
         echo -n " *"
     fi
-    echo -e "\t\t$link" | tee -a SitemapCheck_Report_$label.txt
+    case $code in
+            100 ) Continue="true" ;;
+            101 ) SwitchingProtocol="true" ;;
+            200 ) OK="true" ;;
+            201 ) Created="true" ;;
+            202 ) Accepted="true" ;;
+            203 ) Non-AuthoritativeInformation="true" ;;
+            204 ) NoContent="true" ;;
+            205 ) ResetContent="true" ;;
+            206 ) PartialContent="true" ;;
+            300 ) MultipleChoices="true" ;;
+            301 ) MovedPermanently="true" ;;
+            302 ) Found="true" ;;
+            303 ) SeeOther="true" ;;
+            304 ) NotModified="true" ;;
+            307 ) TemporaryRedirect="true" ;;
+            308 ) PermanentRedirect="true" ;;
+            400 ) BadRequest="true" ;;
+            401 ) Unauthorized="true" ;;
+            403 ) Forbidden="true" ;;
+            404 ) NotFound="true" ;;
+            405 ) MethodNotAllowed="true" ;;
+            406 ) NotAcceptable="true" ;;
+            407 ) ProxyAuthenticationRequired="true" ;;
+            408 ) RequestTimeout="true" ;;
+            409 ) Conflict="true" ;;
+            410 ) Gone="true" ;;
+            411 ) LengthRequired="true" ;;
+            412 ) PreconditionFailed="true" ;;
+            413 ) PayloadTooLarge="true" ;;
+            414 ) URITooLong="true" ;;
+            415 ) UnsupportedMediaType="true" ;;
+            416 ) RangeNotSatisfiable="true" ;;
+            417 ) ExpectationFailed="true" ;;
+            426 ) UpgradeRequired="true" ;;
+            428 ) PreconditionRequired="true" ;;
+            429 ) TooManyRequests="true" ;;
+            431 ) RequestHeaderFieldsTooLarge="true" ;;
+            451 ) UnavailableForLegalReasons="true" ;;
+            500 ) InternalServerError="true" ;;
+            501 ) NotImplemented="true" ;;
+            502 ) BadGateway="true" ;;
+            503 ) ServiceUnavailable="true" ;;
+            504 ) GatewayTimeout="true" ;;
+            505 ) HTTPVersionNotSupported="true" ;;
+            511 ) NetworkAuthenticationRequired="true" ;;
+            * ) ;;
+    esac
+    echo -e "\t\t$link" | tee -a Report.txt
     counter=$[$counter +1]
 done
 
 # sort the report file by status code
-sort -k2 -n -s SitemapCheck_Report_$label.txt > SitemapCheck_Report_$label.tmp; mv SitemapCheck_Report_$label.tmp SitemapCheck_Report_$label.txt
+sort -k2 -n -s Report.txt > Report.tmp; mv Report.tmp Report.txt
+
+# print found anamolies and their meanings
+{
+        echo
+        echo
+        echo "Meaning of HTTP status codes" | tee -a Report.txt
+        if [ -n "$Continue" ];then echo "100: Continue" | tee -a Report.txt; fi
+        if [ -n "$SwitchingProtocol" ];then echo "101: SwitchingProtocol" | tee -a Report.txt; fi
+        if [ -n "$OK" ];then echo "200: OK" | tee -a Report.txt; fi
+        if [ -n "$Created" ];then echo "201: Created" | tee -a Report.txt; fi
+        if [ -n "$Accepted" ];then echo "202: Accepted" | tee -a Report.txt; fi
+        if [ -n "$AuthoritativeInformation" ];then echo "203: Non-AuthoritativeInformation" | tee -a Report.txt; fi
+        if [ -n "$NoContent" ];then echo "204: NoContent" | tee -a Report.txt; fi
+        if [ -n "$ResetContent" ];then echo "205: ResetContent" | tee -a Report.txt; fi
+        if [ -n "$PartialContent" ];then echo "206: PartialContent" | tee -a Report.txt; fi
+        if [ -n "$MultipleChoices" ];then echo "300: MultipleChoices" | tee -a Report.txt; fi
+        if [ -n "$MovedPermanently" ];then echo "301: MovedPermanently" | tee -a Report.txt; fi
+        if [ -n "$Found" ];then echo "302: Found" | tee -a Report.txt; fi
+        if [ -n "$SeeOther" ];then echo "303: SeeOther" | tee -a Report.txt; fi
+        if [ -n "$NotModieq" ];then echo "304: NotModi; fied" | tee -a Report.txt; fi
+        if [ -n "$TemporaryRedirect" ];then echo "307: TemporaryRedirect" | tee -a Report.txt; fi
+        if [ -n "$PermanentRedirect" ];then echo "308: PermanentRedirect" | tee -a Report.txt; fi
+        if [ -n "$BadRequest" ];then echo "400: BadRequest" | tee -a Report.txt; fi
+        if [ -n "$Unauthorized" ];then echo "401: Unauthorized" | tee -a Report.txt; fi
+        if [ -n "$Forbidden" ];then echo "403: Forbidden" | tee -a Report.txt; fi
+        if [ -n "$NotFound" ];then echo "404: NotFound" | tee -a Report.txt; fi
+        if [ -n "$MethodNotAllowed" ];then echo "405: MethodNotAllowed" | tee -a Report.txt; fi
+        if [ -n "$NotAcceptable" ];then echo "406: NotAcceptable" | tee -a Report.txt; fi
+        if [ -n "$ProxyAuthenticationRequired" ];then echo "407: ProxyAuthenticationRequired" | tee -a Report.txt; fi
+        if [ -n "$RequestTimeout" ];then echo "408: RequestTimeout" | tee -a Report.txt; fi
+        if [ -n "$Conflict" ];then echo "409: Conflict" | tee -a Report.txt; fi
+        if [ -n "$Gone" ];then echo "410: Gone" | tee -a Report.txt; fi
+        if [ -n "$LengthRequired" ];then echo "411: LengthRequired" | tee -a Report.txt; fi
+        if [ -n "$PreconditionFailed" ];then echo "412: PreconditionFailed" | tee -a Report.txt; fi
+        if [ -n "$PayloadTooLarge" ];then echo "413: PayloadTooLarge" | tee -a Report.txt; fi
+        if [ -n "$URITooLong" ];then echo "414: URITooLong" | tee -a Report.txt; fi
+        if [ -n "$UnsupportedMediaType" ];then echo "415: UnsupportedMediaType" | tee -a Report.txt; fi
+        if [ -n "$RangeNotSatis" ];then echo "416: RangeNotSatis; fiable" | tee -a Report.txt; fi
+        if [ -n "$ExpectationFailed" ];then echo "417: ExpectationFailed" | tee -a Report.txt; fi
+        if [ -n "$UpgradeRequired" ];then echo "426: UpgradeRequired" | tee -a Report.txt; fi
+        if [ -n "$PreconditionRequired" ];then echo "428: PreconditionRequired" | tee -a Report.txt; fi
+        if [ -n "$TooManyRequests" ];then echo "429: TooManyRequests" | tee -a Report.txt; fi
+        if [ -n "$RequestHeaderTooLarge" ];then echo "431: RequestHeader; fieldsTooLarge" | tee -a Report.txt; fi
+        if [ -n "$UnavailableForLegalReasons" ];then echo "451: UnavailableForLegalReasons" | tee -a Report.txt; fi
+        if [ -n "$InternalServerError" ];then echo "500: InternalServerError" | tee -a Report.txt; fi
+        if [ -n "$NotImplemented" ];then echo "501: NotImplemented" | tee -a Report.txt; fi
+        if [ -n "$BadGateway" ];then echo "502: BadGateway" | tee -a Report.txt; fi
+        if [ -n "$ServiceUnavailable" ];then echo "503: ServiceUnavailable" | tee -a Report.txt; fi
+        if [ -n "$GatewayTimeout" ];then echo "504: GatewayTimeout" | tee -a Report.txt; fi
+        if [ -n "$HTTPVersionNotSupported" ];then echo "505: HTTPVersionNotSupported" | tee -a Report.txt; fi
+        if [ -n "$NetworkAuthenticationRequired" ];then echo "511: Network Authentication Required" | tee -a Report.txt; fi
+}
 
 # print overview
-echo | tee -a SitemapCheck_Report_$label.txt
-echo -e "Sitemap Files:\t$sitemaps" | tee -a SitemapCheck_Report_$label.txt
-echo -e "Links Total:\t$linksTotal" | tee -a SitemapCheck_Report_$label.txt
-echo -e "Links Unique:\t$linksUnique" | tee -a SitemapCheck_Report_$label.txt
-echo -e "Links Checked:\t$linksCheck" | tee -a SitemapCheck_Report_$label.txt
-echo -e "Anomlies:\t$anomalies" | tee -a SitemapCheck_Report_$label.txt
-echo -e "Duration:\t$(echo $SECONDS)s" | tee -a SitemapCheck_Report_$label.txt
-echo | tee -a SitemapCheck_Report_$label.txt
-echo | tee -a SitemapCheck_Report_$label.txt
+echo | tee -a Report.txt
+echo -e "Sitemap Files:\t$sitemaps" | tee -a Report.txt
+echo -e "Links Total:\t$linksTotal" | tee -a Report.txt
+echo -e "Links Unique:\t$linksUnique" | tee -a Report.txt
+echo -e "Links Checked:\t$linksCheck" | tee -a Report.txt
+echo -e "Anomlies:\t$anomalies" | tee -a Report.txt
+echo -e "Duration:\t$(echo $SECONDS)s" | tee -a Report.txt
+echo | tee -a Report.txt
+echo | tee -a Report.txt
 read -p "Done! Press any key to exit ... " -n 1
 clear
