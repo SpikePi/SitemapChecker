51b5c6c2225b703f77f4c505ce27db696ddb20fe
SpikePi <hoffelmann@gmail.com>
2017-04-03 08:28:32 +0200

    refactured script to get better bash language style

--- SitemapChecker.sh
+++ SitemapChecker.sh
@@ -4,20 +4,20 @@
 clear
 
 # Save arguments of command line to variables
-label=$1
-url=$2
-maxNumURLs=$3
+LABEL=$1
+URL=$2
+MAXNUMURLS=$3
 
 # label your testrun
-if [ -z $1 ]; then
-        read -p "Type a name for your testrun: " label
+if [[ -z $1 ]]; then
+        read -p "Type a name for your testrun: " LABEL
 fi
 
 # check if seccessary tools are installed
 # if not install them (made it work on debian based distros and fedora)
 which xmllint curl wget sort sed grep > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-        distribution=$(lsb_release -is)
+if [[ $? -ne 0 ]]; then
+        DISTRIBUTION=$(lsb_release -is)
 
         if [ $? -ne 0 ]; then
                 echo "Cant't identify your Linux Distribution because lsb_release is not installed."
@@ -25,7 +25,7 @@ if [ $? -ne 0 ]; then
                 exit 1
         fi
 
-        case $distribution in
+        case ${DISTRIBUTION}} in
                 Debian | LinuxMint | Ubuntu )
                         sudo apt-get update; sudo apt-get install curl libxml2-utils wget coreutils sed grep
                         ;;
@@ -43,23 +43,23 @@ fi
 
 
 # if argument 1 is empty ask for input
-if [ -z $url ]; then
+if [[ -z ${URL} ]]; then
         echo "If you don't know where the sitemap is, look for it at the robots.txt"
-        read -p "Type the URL you want to check: " url
+        read -p "Type the URL you want to check: " URL
         clear
 fi
 
 # clean remainings of previous run
-rm ~/SitemapCheck_$label/* 2> /dev/null
+rm ~/SitemapCheck_${LABEL}/* 2> /dev/null
 
 # create a folder for the files we need to check
-mkdir ~/SitemapCheck_$label 2> /dev/null
+mkdir ~/SitemapCheck_${LABEL} 2> /dev/null
 # go into the directory we want to work with
-cd ~/SitemapCheck_$label
+cd ~/SitemapCheck_${LABEL}
 
 # download the sitemap.xml. In case of an error exit the script
-wget $url -O sitemap.tmp
-if [ -z $? ]; then
+wget ${URL} -O sitemap.tmp
+if [[ -z $? ]]; then
         clear
         echo "No sitemap found!"
         echo
@@ -70,8 +70,7 @@ xmllint --format sitemap.tmp > sitemap.xml
 
 # look if there are other sitemaps linked
 # if sitemap is split into pieces get all parts of it
-grep "\.xml" sitemap.xml
-if [ $? -eq 0 ]; then
+if grep "\.xml" sitemap.xml; then
         # save all found sitemap addresses in new file
         grep -Eo "<loc>.*\.xml.*</loc>" sitemap.xml > sitemaps.tmp
 
@@ -86,7 +85,7 @@ if [ $? -eq 0 ]; then
 fi
 
 # save how many sitemap-files were found
-sitemaps=$(ls *.xml* 2> /dev/null | wc -l)
+SITEMAPS=$(ls *.xml* 2> /dev/null | wc -l)
 
 # filter all downloaded sitemaps for URLs and put them in one file
 grep -Eho "(http[s]*:\/\/www\.)([a-zA-Z0-9_-]{4,}\.)([a-zA-Z]{2,3})([a-zA-Z0-9\/_\.\?-])+" *.xml > sitemap_links_all.tmp
@@ -96,11 +95,11 @@ grep -Eho "(http[s]*:\/\/www\.)([a-zA-Z0-9_-]{4,}\.)([a-zA-Z]{2,3})([a-zA-Z0-9\/
 #sed -i 's/<[\/]*loc>//g' sitemap_links_all.tmp
 
 # save how many links were found
-linksTotal=$(wc -l < sitemap_links_all.tmp)
+LINKSTOTAL=$(wc -l < sitemap_links_all.tmp)
 
 # sort all links to remove duplicats
 sort -u sitemap_links_all.tmp > sitemap_links_all.txt
-linksUnique=$(wc -l < sitemap_links_all.txt)
+LINKSUNIQUE=$(wc -l < sitemap_links_all.txt)
 
 # shuffle links to get a random set of links from every part of the site
 shuf sitemap_links_all.txt > sitemap_links_all.tmp
@@ -112,95 +111,95 @@ clear
 date | tee Report.txt
 
 # check if there was a second argument for maxNumURLs
-if [ -z $3 ]; then
+if [[ -z $3 ]]; then
         # print how many unique links were found
         echo
-        echo -e "Found $linksUnique unique links.\n"
+        echo -e "Found ${LINKSUNIQUE} unique links.\n"
 
         # ask how many of the links should be checked
-        read -p "How many links should be checked? (Press ENTER to check all) " maxNumURLs
+        read -p "How many links should be checked? (Press ENTER to check all) " MAXNUMURLS
 else
-        echo "Testinging $maxNumURLs Links out of $linksUnique."
+        echo "Testinging ${MAXNUMURLS} Links out of ${LINKSUNIQUE}."
 fi
-if [ -z $maxNumURLs ]; then maxNumURLs=999999; fi
+if [[ -z ${MAXNUMURLS} ]]; then maxNumURLs=999999; fi
 
 # take first ${argument2} links that should be tested and sort them again
 # so that they are tested in alphabetic order to get a better output
-head -n $maxNumURLs sitemap_links_all.tmp | sort > sitemap_links_part.txt
+head -n ${MAXNUMURLS} sitemap_links_all.tmp | sort > sitemap_links_part.txt
 
 # clean xml- and temp-files
 rm *.tmp
 rm *.xml
 
 # set some variables to get a nice output
-counter=0
-anomalies=0
+COUNTER=0
+ANOMALIES=0
 SECONDS=0
-linksCheck=$(wc -l < sitemap_links_part.txt)
+LINKSTOCHECK=$(wc -l < sitemap_links_part.txt)
 
 # create the header of the Report
 echo -e "\nTest\tResponse\tURL" | tee -a Report.txt
 
 # loop through all links in the file and test them
-for link in $(cat sitemap_links_part.txt); do
-        counter=$(( $counter + 1 ))
-        echo -en "$counter/$linksCheck\t" | tee -a Report.txt
-        code=$(curl --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36" -L --max-time 10 --connect-timeout 5 --silent --head -o /dev/null --write-out "%{http_code}" $link)
-        echo -n $code | tee -a Report.txt
-        if [ $code -ne 200 ]; then
-                anomalies=$(( $anomalies + 1 ))
+for LINK in $(cat sitemap_links_part.txt); do
+        COUNTER=$(( ${COUNTER} + 1 ))
+        echo -en "${COUNTER}/${LINKSTOCHECK}\t" | tee -a Report.txt
+        CODE=$(curl --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36" -L --max-time 10 --connect-timeout 5 --silent --head -o /dev/null --write-out "%{http_code}" ${LINK})
+        echo -n ${CODE} | tee -a Report.txt
+        if [ ${CODE} -ne 200 ]; then
+                ANOMALIES=$(( ${ANOMALIES} + 1 ))
                 echo -n " *"
         fi
-        case $code in
-                000 ) curl="true" ;;
-                100 ) Continue="true" ;;
-                101 ) SwitchingProtocol="true" ;;
+        case ${CODE} in
+                000 ) CURL="true" ;;
+                100 ) CONTINUE="true" ;;
+                101 ) SWITCHINGPROTOCOL="true" ;;
                 200 ) OK="true" ;;
-                201 ) Created="true" ;;
-                202 ) Accepted="true" ;;
-                203 ) Non-AuthoritativeInformation="true" ;;
-                204 ) NoContent="true" ;;
-                205 ) ResetContent="true" ;;
-                206 ) PartialContent="true" ;;
-                300 ) MultipleChoices="true" ;;
-                301 ) MovedPermanently="true" ;;
-                302 ) Found="true" ;;
-                303 ) SeeOther="true" ;;
-                304 ) NotModified="true" ;;
-                307 ) TemporaryRedirect="true" ;;
-                308 ) PermanentRedirect="true" ;;
-                400 ) BadRequest="true" ;;
-                401 ) Unauthorized="true" ;;
-                403 ) Forbidden="true" ;;
-                404 ) NotFound="true" ;;
-                405 ) MethodNotAllowed="true" ;;
-                406 ) NotAcceptable="true" ;;
-                407 ) ProxyAuthenticationRequired="true" ;;
-                408 ) RequestTimeout="true" ;;
-                409 ) Conflict="true" ;;
-                410 ) Gone="true" ;;
-                411 ) LengthRequired="true" ;;
-                412 ) PreconditionFailed="true" ;;
-                413 ) PayloadTooLarge="true" ;;
-                414 ) URITooLong="true" ;;
-                415 ) UnsupportedMediaType="true" ;;
-                416 ) RangeNotSatisfiable="true" ;;
-                417 ) ExpectationFailed="true" ;;
-                426 ) UpgradeRequired="true" ;;
-                428 ) PreconditionRequired="true" ;;
-                429 ) TooManyRequests="true" ;;
-                431 ) RequestHeaderFieldsTooLarge="true" ;;
-                451 ) UnavailableForLegalReasons="true" ;;
-                500 ) InternalServerError="true" ;;
-                501 ) NotImplemented="true" ;;
-                502 ) BadGateway="true" ;;
-                503 ) ServiceUnavailable="true" ;;
-                504 ) GatewayTimeout="true" ;;
-                505 ) HTTPVersionNotSupported="true" ;;
-                511 ) NetworkAuthenticationRequired="true" ;;
-                * ) Unknown="true" ;;
+                201 ) CREATED="true" ;;
+                202 ) ACCEPTED="true" ;;
+                203 ) NON-AuthoritativeInformation="true" ;;
+                204 ) NOCONTENT="true" ;;
+                205 ) RESETCONTENT="true" ;;
+                206 ) PARTIALCONTENT="true" ;;
+                300 ) MULTIPLECHOICES="true" ;;
+                301 ) MOVEDPERMANENTLY="true" ;;
+                302 ) FOUND="true" ;;
+                303 ) SEEOTHER="true" ;;
+                304 ) NOTMODIFIED="true" ;;
+                307 ) TEMPORARYREDIRECT="true" ;;
+                308 ) PERMANENTREDIRECT="true" ;;
+                400 ) BADREQUEST="true" ;;
+                401 ) UNAUTHORIZED="true" ;;
+                403 ) FORBIDDEN="true" ;;
+                404 ) NOTFOUND="true" ;;
+                405 ) METHODNOTALLOWED="true" ;;
+                406 ) NOTACCEPTABLE="true" ;;
+                407 ) PROXYAUTHENTICATIONREQUIRED="true" ;;
+                408 ) REQUESTTIMEOUT="true" ;;
+                409 ) CONFLICT="true" ;;
+                410 ) GONE="true" ;;
+                411 ) LENGTHREQUIRED="true" ;;
+                412 ) PRECONDITIONFAILED="true" ;;
+                413 ) PAYLOADTOOLARGE="true" ;;
+                414 ) URITOOLONG="true" ;;
+                415 ) UNSUPPORTEDMEDIATYPE="true" ;;
+                416 ) RANGENOTSATISFIABLE="true" ;;
+                417 ) EXPECTATIONFAILED="true" ;;
+                426 ) UPGRADEREQUIRED="true" ;;
+                428 ) PRECONDITIONREQUIRED="true" ;;
+                429 ) TOOMANYREQUESTS="true" ;;
+                431 ) REQUESTHEADERFIELDSTOOLARGE="true" ;;
+                451 ) UNAVAILABLEFORLEGALREASONS="true" ;;
+                500 ) INTERNALSERVERERROR="true" ;;
+                501 ) NOTIMPLEMENTED="true" ;;
+                502 ) BADGATEWAY="true" ;;
+                503 ) SERVICEUNAVAILABLE="true" ;;
+                504 ) GATEWAYTIMEOUT="true" ;;
+                505 ) HTTPVERSIONNOTSUPPORTED="true" ;;
+                511 ) NETWORKAUTHENTICATIONREQUIRED="true" ;;
+                * ) UNKNOWN="true" ;;
         esac
-        echo -e "\t\t$link" | tee -a Report.txt
+        echo -e "\t\t${LINK}" | tee -a Report.txt
 done
 
 # sort the report file by status code
@@ -208,61 +207,61 @@ sort -k2 -n -s Report.txt > Report.tmp; mv Report.tmp Report.txt
 
 # print found status codes and their meanings
 echo -e "\n\nMeaning of HTTP status codes" | tee -a Report.txt
-if [ -n "$curl" ];then echo "000: cURL did not receive a HTTP response code" | tee -a Report.txt; fi
-if [ -n "$Continue" ];then echo "100: Continue" | tee -a Report.txt; fi
-if [ -n "$SwitchingProtocol" ];then echo "101: SwitchingProtocol" | tee -a Report.txt; fi
-if [ -n "$OK" ];then echo "200: OK" | tee -a Report.txt; fi
-if [ -n "$Created" ];then echo "201: Created" | tee -a Report.txt; fi
-if [ -n "$Accepted" ];then echo "202: Accepted" | tee -a Report.txt; fi
-if [ -n "$AuthoritativeInformation" ];then echo "203: Non-AuthoritativeInformation" | tee -a Report.txt; fi
-if [ -n "$NoContent" ];then echo "204: NoContent" | tee -a Report.txt; fi
-if [ -n "$ResetContent" ];then echo "205: ResetContent" | tee -a Report.txt; fi
-if [ -n "$PartialContent" ];then echo "206: PartialContent" | tee -a Report.txt; fi
-if [ -n "$MultipleChoices" ];then echo "300: MultipleChoices" | tee -a Report.txt; fi
-if [ -n "$MovedPermanently" ];then echo "301: MovedPermanently" | tee -a Report.txt; fi
-if [ -n "$Found" ];then echo "302: Found" | tee -a Report.txt; fi
-if [ -n "$SeeOther" ];then echo "303: SeeOther" | tee -a Report.txt; fi
-if [ -n "$NotModieq" ];then echo "304: NotModi; fied" | tee -a Report.txt; fi
-if [ -n "$TemporaryRedirect" ];then echo "307: TemporaryRedirect" | tee -a Report.txt; fi
-if [ -n "$PermanentRedirect" ];then echo "308: PermanentRedirect" | tee -a Report.txt; fi
-if [ -n "$BadRequest" ];then echo "400: BadRequest" | tee -a Report.txt; fi
-if [ -n "$Unauthorized" ];then echo "401: Unauthorized" | tee -a Report.txt; fi
-if [ -n "$Forbidden" ];then echo "403: Forbidden" | tee -a Report.txt; fi
-if [ -n "$NotFound" ];then echo "404: NotFound" | tee -a Report.txt; fi
-if [ -n "$MethodNotAllowed" ];then echo "405: MethodNotAllowed" | tee -a Report.txt; fi
-if [ -n "$NotAcceptable" ];then echo "406: NotAcceptable" | tee -a Report.txt; fi
-if [ -n "$ProxyAuthenticationRequired" ];then echo "407: ProxyAuthenticationRequired" | tee -a Report.txt; fi
-if [ -n "$RequestTimeout" ];then echo "408: RequestTimeout" | tee -a Report.txt; fi
-if [ -n "$Conflict" ];then echo "409: Conflict" | tee -a Report.txt; fi
-if [ -n "$Gone" ];then echo "410: Gone" | tee -a Report.txt; fi
-if [ -n "$LengthRequired" ];then echo "411: LengthRequired" | tee -a Report.txt; fi
-if [ -n "$PreconditionFailed" ];then echo "412: PreconditionFailed" | tee -a Report.txt; fi
-if [ -n "$PayloadTooLarge" ];then echo "413: PayloadTooLarge" | tee -a Report.txt; fi
-if [ -n "$URITooLong" ];then echo "414: URITooLong" | tee -a Report.txt; fi
-if [ -n "$UnsupportedMediaType" ];then echo "415: UnsupportedMediaType" | tee -a Report.txt; fi
-if [ -n "$RangeNotSatis" ];then echo "416: RangeNotSatis; fiable" | tee -a Report.txt; fi
-if [ -n "$ExpectationFailed" ];then echo "417: ExpectationFailed" | tee -a Report.txt; fi
-if [ -n "$UpgradeRequired" ];then echo "426: UpgradeRequired" | tee -a Report.txt; fi
-if [ -n "$PreconditionRequired" ];then echo "428: PreconditionRequired" | tee -a Report.txt; fi
-if [ -n "$TooManyRequests" ];then echo "429: TooManyRequests" | tee -a Report.txt; fi
-if [ -n "$RequestHeaderTooLarge" ];then echo "431: RequestHeader; fieldsTooLarge" | tee -a Report.txt; fi
-if [ -n "$UnavailableForLegalReasons" ];then echo "451: UnavailableForLegalReasons" | tee -a Report.txt; fi
-if [ -n "$InternalServerError" ];then echo "500: InternalServerError" | tee -a Report.txt; fi
-if [ -n "$NotImplemented" ];then echo "501: NotImplemented" | tee -a Report.txt; fi
-if [ -n "$BadGateway" ];then echo "502: BadGateway" | tee -a Report.txt; fi
-if [ -n "$ServiceUnavailable" ];then echo "503: ServiceUnavailable" | tee -a Report.txt; fi
-if [ -n "$GatewayTimeout" ];then echo "504: GatewayTimeout" | tee -a Report.txt; fi
-if [ -n "$HTTPVersionNotSupported" ];then echo "505: HTTPVersionNotSupported" | tee -a Report.txt; fi
-if [ -n "$NetworkAuthenticationRequired" ];then echo "511: Network Authentication Required" | tee -a Report.txt; fi
-if [ -n "$Unknown" ];then echo "Unknown HTTP response code" | tee -a Report.txt; fi
+if [[ -n "$CURL" ]];then echo "000: cURL did not receive a HTTP response code" | tee -a Report.txt; fi
+if [[ -n "$CONTINUE" ]];then echo "100: Continue" | tee -a Report.txt; fi
+if [[ -n "$SWITCHINGPROTOCOL" ]];then echo "101: SwitchingProtocol" | tee -a Report.txt; fi
+if [[ -n "$OK" ]];then echo "200: OK" | tee -a Report.txt; fi
+if [[ -n "$CREATED" ]];then echo "201: Created" | tee -a Report.txt; fi
+if [[ -n "$ACCEPTED" ]];then echo "202: Accepted" | tee -a Report.txt; fi
+if [[ -n "$AUTHORITATIVEINFORMATION" ]];then echo "203: Non-AuthoritativeInformation" | tee -a Report.txt; fi
+if [[ -n "$NOCONTENT" ]];then echo "204: NoContent" | tee -a Report.txt; fi
+if [[ -n "$RESETCONTENT" ]];then echo "205: ResetContent" | tee -a Report.txt; fi
+if [[ -n "$PARTIALCONTENT" ]];then echo "206: PartialContent" | tee -a Report.txt; fi
+if [[ -n "$MULTIPLECHOICES" ]];then echo "300: MultipleChoices" | tee -a Report.txt; fi
+if [[ -n "$MOVEDPERMANENTLY" ]];then echo "301: MovedPermanently" | tee -a Report.txt; fi
+if [[ -n "$FOUND" ]];then echo "302: Found" | tee -a Report.txt; fi
+if [[ -n "$SEEOTHER" ]];then echo "303: SeeOther" | tee -a Report.txt; fi
+if [[ -n "$NOTMODIEQ" ]];then echo "304: NotModi; fied" | tee -a Report.txt; fi
+if [[ -n "$TEMPORARYREDIRECT" ]];then echo "307: TemporaryRedirect" | tee -a Report.txt; fi
+if [[ -n "$PERMANENTREDIRECT" ]];then echo "308: PermanentRedirect" | tee -a Report.txt; fi
+if [[ -n "$BADREQUEST" ]];then echo "400: BadRequest" | tee -a Report.txt; fi
+if [[ -n "$UNAUTHORIZED" ]];then echo "401: Unauthorized" | tee -a Report.txt; fi
+if [[ -n "$FORBIDDEN" ]];then echo "403: Forbidden" | tee -a Report.txt; fi
+if [[ -n "$NOTFOUND" ]];then echo "404: NotFound" | tee -a Report.txt; fi
+if [[ -n "$METHODNOTALLOWED" ]];then echo "405: MethodNotAllowed" | tee -a Report.txt; fi
+if [[ -n "$NOTACCEPTABLE" ]];then echo "406: NotAcceptable" | tee -a Report.txt; fi
+if [[ -n "$PROXYAUTHENTICATIONREQUIRED" ]];then echo "407: ProxyAuthenticationRequired" | tee -a Report.txt; fi
+if [[ -n "$REQUESTTIMEOUT" ]];then echo "408: RequestTimeout" | tee -a Report.txt; fi
+if [[ -n "$CONFLICT" ]];then echo "409: Conflict" | tee -a Report.txt; fi
+if [[ -n "$GONE" ]];then echo "410: Gone" | tee -a Report.txt; fi
+if [[ -n "$LENGTHREQUIRED" ]];then echo "411: LengthRequired" | tee -a Report.txt; fi
+if [[ -n "$PRECONDITIONFAILED" ]];then echo "412: PreconditionFailed" | tee -a Report.txt; fi
+if [[ -n "$PAYLOADTOOLARGE" ]];then echo "413: PayloadTooLarge" | tee -a Report.txt; fi
+if [[ -n "$URITOOLONG" ]];then echo "414: URITooLong" | tee -a Report.txt; fi
+if [[ -n "$UNSUPPORTEDMEDIATYPE" ]];then echo "415: UnsupportedMediaType" | tee -a Report.txt; fi
+if [[ -n "$RANGENOTSATIS" ]];then echo "416: RangeNotSatis; fiable" | tee -a Report.txt; fi
+if [[ -n "$EXPECTATIONFAILED" ]];then echo "417: ExpectationFailed" | tee -a Report.txt; fi
+if [[ -n "$UPGRADEREQUIRED" ]];then echo "426: UpgradeRequired" | tee -a Report.txt; fi
+if [[ -n "$PRECONDITIONREQUIRED" ]];then echo "428: PreconditionRequired" | tee -a Report.txt; fi
+if [[ -n "$TOOMANYREQUESTS" ]];then echo "429: TooManyRequests" | tee -a Report.txt; fi
+if [[ -n "$REQUESTHEADERTOOLARGE" ]];then echo "431: RequestHeader; fieldsTooLarge" | tee -a Report.txt; fi
+if [[ -n "$UNAVAILABLEFORLEGALREASONS" ]];then echo "451: UnavailableForLegalReasons" | tee -a Report.txt; fi
+if [[ -n "$INTERNALSERVERERROR" ]];then echo "500: InternalServerError" | tee -a Report.txt; fi
+if [[ -n "$NOTIMPLEMENTED" ]];then echo "501: NotImplemented" | tee -a Report.txt; fi
+if [[ -n "$BADGATEWAY" ]];then echo "502: BadGateway" | tee -a Report.txt; fi
+if [[ -n "$SERVICEUNAVAILABLE" ]];then echo "503: ServiceUnavailable" | tee -a Report.txt; fi
+if [[ -n "$GATEWAYTIMEOUT" ]];then echo "504: GatewayTimeout" | tee -a Report.txt; fi
+if [[ -n "$HTTPVERSIONNOTSUPPORTED" ]];then echo "505: HTTPVersionNotSupported" | tee -a Report.txt; fi
+if [[ -n "$NETWORKAUTHENTICATIONREQUIRED" ]];then echo "511: Network Authentication Required" | tee -a Report.txt; fi
+if [[ -n "$UNKNOWN" ]];then echo "Unknown HTTP response code" | tee -a Report.txt; fi
 
 # print overview
 echo | tee -a Report.txt
-echo -e "Sitemap Files:\t$sitemaps" | tee -a Report.txt
-echo -e "Links Total:\t$linksTotal" | tee -a Report.txt
-echo -e "Links Unique:\t$linksUnique" | tee -a Report.txt
-echo -e "Links Checked:\t$counter" | tee -a Report.txt
-echo -e "Anomalies:\t$anomalies" | tee -a Report.txt
-echo -e "Duration:\t$SECONDS s\n\n" | tee -a Report.txt
+echo -e "Sitemap Files:\t${SITEMAPS}" | tee -a Report.txt
+echo -e "Links Total:\t${LINKSTOTAL}" | tee -a Report.txt
+echo -e "Links Unique:\t${LINKSUNIQUE}" | tee -a Report.txt
+echo -e "Links Checked:\t${COUNTER}" | tee -a Report.txt
+echo -e "Anomalies:\t${ANOMALIES}" | tee -a Report.txt
+echo -e "Duration:\t${SECONDS} s\n\n" | tee -a Report.txt
 read -p "Done! Press any key to exit ... " -n 1
 clear
